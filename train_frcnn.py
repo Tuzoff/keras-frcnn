ae268a43434795f2dd60309f893fdb4952db3f8c
Max Kharchenko <ma.kharchenko@gmail.com>
2017-05-04 09:52:53 +0000

    Return init and weights load

--- train_frcnn.py
+++ train_frcnn.py
@@ -65,8 +65,8 @@ C.rot_90 = options.rot_90
 
 C.model_path = options.output_weight_path
 
-#if options.input_weight_path:
-#	C.base_net_weights = options.input_weight_path
+if options.input_weight_path:
+	C.base_net_weights = options.input_weight_path
 
 all_imgs, classes_count, class_mapping = get_data(options.train_path)
 
